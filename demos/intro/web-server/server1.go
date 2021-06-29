62861e78634cacba972f38579f517bd69907ba50
Ignacio Herrera <ignacioromanherrera@gmail.com>
2021-06-29 00:17:40 -0300

    Demos/Intro/Web Server header details

--- demos/intro/web-server/server1.go
+++ demos/intro/web-server/server1.go
@@ -14,5 +14,21 @@ func main() {
 
 // handler echoes the Path component of the requested URL
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
